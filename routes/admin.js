f9179fbbff0deaa891bbe8da14c5ab8cd7147e70
mkdirharsh108 <mkdir.harsh108@gmail.com>
2026-02-10 11:11:03 +0530

    establish adminRouter

---
+++ routes/admin.js
@@ -0,0 +1,26 @@
+const { Router } = require("express");
+const adminRouter = Router();
+
+// adminRouter.use(adminMiddleware) when need more
+
+adminRouter.post("/signup", function(req, res){
+    res.json({
+        message : "admin signup endpoint"
+    })
+});
+
+adminRouter.post("/signin", function(req, res){
+    res.json({
+        message : "admin signin endpoint"
+    })
+});
+
+adminRouter.get("/purchases", function(req, res){
+    res.json({
+        message : "admin ji !! You have purchased eerything!!"
+    })
+});
+
+module.exports = {
+    adminRouter: adminRouter
+}
