65928c9155a68399c28d9d9c732c94ae720c1bc5
mkdirharsh108 <mkdir.harsh108@gmail.com>
2026-02-10 19:47:15 +0530

    added Database Schema

---
+++ db.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+const ObjectId = mongoose.Types.ObjectId;
+
+const userSchema = new Schema({
+    email : { type: String, unique: true},
+    password: String,
+    firstName: String,
+    lastName: String,
+});
+
+const courseSchema = new Schema({
+    title: String,
+    description: String,
+    price: Number,
+    imageUrl: String,
+    creatorId: ObjectId,
+});
+
+const purchaseSchema = new Schema({
+    userId: ObjectId,
+    courseId: ObjectId
+});
+
+const adminSchema = new Schema({
+    email : { type: String, unique: true},
+    password: String,
+    firstName: String,
+    lastName: String,    
+});
+
+const userModel = mongoose.model("user", userSchema);
+const courseModel = mongoose.model("course", courseSchema);
+const purchaseModel = mongoose.model("purchase", purchaseSchema);
+const adminModel = mongoose.model("admin", adminSchema);
+
+module.exports = {
+    userModel,
+    courseModel,
+    purchaseModel,
+    adminModel
+}
