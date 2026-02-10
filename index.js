7756eb3ddbbe7ba86f09cc3b21bee45f9c2f12b4
mkdirharsh108 <mkdir.harsh108@gmail.com>
2026-02-10 19:47:36 +0530

    linked databases

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
 const { userRouter } = require("./routes/user");
 const { courseRouter } = require("./routes/course");
 const { adminRouter } = require("./routes/admin");
+const { default: mongoose } = require("mongoose");
 const app = express(); 
 
 app.use("/user", userRouter);
 app.use("/course", courseRouter);
 app.use("/admin", adminRouter);
 
-
-
-
-
-
-app.listen(3000);
+async function main(){
+    await mongoose.connect("")
+    app.listen(3000);
+    console.log("listing on port : 3000")
+}
+main()
