6307191386ce472e162ada31ec4dd39a54e237b7
mkdirharsh108 <mkdir.harsh108@gmail.com>
2026-02-11 17:07:25 +0530

    added middleware and completed  post course endpoint

--- routes/admin.js
+++ routes/admin.js
@@ -1,23 +1,74 @@
 const { Router } = require("express");
 const adminRouter = Router();
-const { adminModel } = require("../db");
+const { adminModel, courseModel } = require("../db");
+const jwt = require("jsonwebtoken");
 // adminRouter.use(adminMiddleware) when need more
+const { JET_ADMIN_PASSWORD } = require("../config");
+const { adminMiddleware } = require("../middleware/admin");
 
-adminRouter.post("/signup", function(req, res){
-    res.json({
-        message : "admin signup endpoint"
-    })
-});
 
-adminRouter.post("/signin", function(req, res){
-    res.json({
-        message : "admin signin endpoint"
+adminRouter.post("/signup", async function(req, res){
+    const { email, password, firstName, lastName } = req.body;
+    
+    await adminModel.create({
+        email, // == emial: email
+        password,
+        firstName,
+        lastName
     })
-});
+    res.json({
+        message : "signup succeeded"
+    })
+})
+
+adminRouter.post("/signin", async function(req, res){
+    const { email, password } = req.body;
+    const admin = await adminModel.findOne({ // Implement bcrypt further in future 
+        // either use findOne or else use user.length === 1 
+        email: email,
+        password: password
+    })
+
+    if(admin) {
+        const token = jwt.sign({
+            id: admin._id
+
+        }, JWT_USER_PASSWORD);
+        
+        res.json({
+            token: token
+        })
+    } else {
+        res.status(403).json({
+            message : "Incorrect credentials"
+        })
+    }
+})
 
 adminRouter.get("/purchases", function(req, res){
     res.json({
         message : "admin ji !! You have purchased eerything!!"
+    })
+});
+
+adminRouter.post("/course", adminMiddleware, async function(req, res){
+    const adminId  = req.userId;
+    const {title, description, imageUrl, price } = req.body;
+
+    // for image as image only not url watch
+    // creating a web3 saas in 6 hours
+    const course = await courseModel.create({
+        title, description, imageUrl, price, creatorId: adminId
+    })
+    res.json({
+        message : "Course Created",
+        courseId: courseModel._id
+    })
+});
+
+adminRouter.put("/course", function(req, res){
+    res.json({
+        message : "put course endpoint"
     })
 });
 
