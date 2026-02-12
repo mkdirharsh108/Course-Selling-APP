34c25f217a1b8cc078a33e6a7fcaabe2a03b6cd1
mkdirharsh108 <mkdir.harsh108@gmail.com>
2026-02-12 22:17:37 +0530

    linked frontend using path

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
+require('dotenv').config()
+console.log(process.env.MONGO_URL)
 const express = require("express");
 const mongoose = require("mongoose");
 
 const { userRouter } = require("./routes/user");
 const { courseRouter } = require("./routes/course");
 const { adminRouter } = require("./routes/admin");
-const { default: mongoose } = require("mongoose");
 const app = express(); 
+
+app.use(express.json());
+
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "frontend")));
+
 
 app.use("/user", userRouter);
 app.use("/course", courseRouter);
 app.use("/admin", adminRouter);
-
+    
 async function main(){
-    await mongoose.connect("")
+    await mongoose.connect(process.env.MONGO_URL)
     app.listen(3000);
     console.log("listing on port : 3000")
 }
